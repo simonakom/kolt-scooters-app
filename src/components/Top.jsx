d40b0ea41578536154e6b0e93452ef676481cef2
simonakomarovska <simonak399@gmail.com>
2024-02-28 21:25:10 +0100

    img

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -5,7 +5,7 @@ import * as PropTypes from "prop-types";
 export default function Top ({notifyScooterAddition}) {
   return (
     <div className="mx-auto bg-teal-700 min-h-[300px] min-w-[100px] p-4 flex flex-col items-center overflow-hidden"> 
-      <img src="../public/electric-scooter.png" alt="Scooter" className="max-w-[100px] mt-5 sm:max-w-[150px] sm:mt-5"/>
+      <img src="/public/electric-scooter.png" alt="Scooter" className="max-w-[100px] mt-5 sm:max-w-[150px] sm:mt-5"/>
       <h2 className='my-7 text-3xl font-semibold text-white'> Kolt ScootTrack</h2>
       <AddScooterForm notifyScooterAddition={notifyScooterAddition} />
     </div>
